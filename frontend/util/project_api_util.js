2a8741143a04774485069d937294c5ca25d59714
Danny Morgan <djmorgan2412@gmail.com>
2020-06-06 13:54:08 -0400

    all

---
+++ frontend/util/project_api_util.js
@@ -0,0 +1,36 @@
+export const fetchProjects = () => (
+    $.ajax({
+        url: '/api/projects'
+    })
+)
+
+export const fetchProject = id => (
+    $.ajax({
+        url: `/api/projects/${id}`
+    })
+)
+
+export const createProject = project => (
+    $.ajax({
+        url: '/api/projects',
+        method: 'Post',
+        data: { project },
+        contentType: false,
+        processData: false
+    })
+)
+
+export const updateProject = project => (
+    $.ajax({
+        url: `/api/projects/${project.id}`,
+        method: 'Patch',
+        data: { project }
+    })
+)
+
+export const deleteProject = id => (
+    $.ajax({
+        url: '/api/projects',
+        method: 'Delete'
+    })
+)
